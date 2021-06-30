43810e92a0d39553449c2a0ad4bf8dbb267c4e07
Samyak Mehta <samyakmehta28@gmail.com>
2021-06-30 17:58:54 +0530

    Dynamic display

--- todolist/views.py
+++ todolist/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from .models import TodoList
 # Create your views here.
 
 def index(request):
-    return render(request,'todolist/index.html')
+    todo_items = TodoList.objects.order_by('id')
+    context = {'todo_items' : todo_items}
+    return render(request,'todolist/index.html',context)
