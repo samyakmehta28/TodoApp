e9044adda556658bba188085eac4f65a7692036d
Samyak Mehta <samyakmehta28@gmail.com>
2021-06-30 21:52:54 +0530

    changes to font family

--- todolist/forms.py
+++ todolist/forms.py
@@ -3,4 +3,4 @@ from django import forms
 class TodoListForm(forms.Form):
     text = forms.CharField(max_length=60, 
                            widget=forms.TextInput(
-                              attrs={'class': 'form-control','placeholder' : 'Enter todo e.g. Homework', 'aria-label': 'Todo', 'aria-describeby' : 'add-btn'}))
+                              attrs={'class': 'form-control','placeholder' : 'Enter todo e.g. Homework', 'style': 'font-weight : bold', 'aria-label': 'Todo', 'aria-describeby' : 'add-btn'}))
