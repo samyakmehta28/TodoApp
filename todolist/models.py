02ec2d174c5704367f8ceda82e49cc9d980aa984
Samyak Mehta <samyakmehta28@gmail.com>
2021-07-23 16:57:37 +0530

    final modifications

--- todolist/models.py
+++ todolist/models.py
@@ -6,8 +6,6 @@ from django.conf import settings
 class TodoList(models.Model):
     text = models.CharField(max_length=60)
     completed = models.BooleanField(default=False)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    #created_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, related_name="todo_created_by", on_delete=models.CASCADE)
     
     def __str__(self):
         return self.text
--- todolist/views.py
+++ todolist/views.py
@@ -6,7 +6,7 @@ from django.views.decorators.http import require_POST
 # Create your views here.
 
 def index(request):
-    todo_items = TodoList.objects.filter(author = request.user).order_by('id')
+    todo_items = TodoList.objects.order_by('id')
     form = TodoListForm()
     context = {'todo_items' : todo_items, 'form' : form}
     return render(request,'todolist/index.html',context)
@@ -15,7 +15,7 @@ def index(request):
 def addTodoItem(request):
     form = TodoListForm(request.POST)
     if form.is_valid():
-        new_todo = TodoList(text=request.POST['text'], author=request.user)
+        new_todo = TodoList(text=request.POST['text'])
         new_todo.save()
     return redirect('index')
 
@@ -26,9 +26,9 @@ def completedTodo(request,todo_id):
     return redirect('index')
 
 def deleteCompletedTodo(request):
-    TodoList.objects.filter(author = request.user).filter(completed__exact=True).delete()
+    TodoList.objects.filter(completed__exact=True).delete()
     return redirect('index')
 
 def deleteAllTodo(request):
-    TodoList.objects.filter(author = request.user).delete()
+    TodoList.objects.all().delete()
     return redirect('index')
